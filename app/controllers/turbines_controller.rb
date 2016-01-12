7b320c5890d16b412dcf7faf922e375df34097ef
Brendan Lewis <lewis2ba@dukes.jmu.edu>
2016-01-12 14:42:19 -0500

    getWeatherData talking to wunderground API

--- app/controllers/turbines_controller.rb
+++ app/controllers/turbines_controller.rb
@@ -1,4 +1,5 @@
 class TurbinesController < ApplicationController
+# before_action :authenticate_user!
 
   def index
     @turbines = Turbine.all()
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
 class UsersController < ApplicationController
-
+# for user profiles
 
 end
